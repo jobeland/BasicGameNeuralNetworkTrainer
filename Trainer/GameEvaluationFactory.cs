dfaa7a7e50a27f5be44c37dadcbff5800478cb76
jobeland <jordan.beland@gmail.com>
2015-09-07 22:36:54 -0700

    add files from NeuralNetwork repo

---
+++ Trainer/GameEvaluationFactory.cs
@@ -0,0 +1,20 @@
+﻿using ArtificialNeuralNetwork;
+using BasicGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralNetwork.GeneticAlgorithm;
+using NeuralNetwork.GeneticAlgorithm.Evaluatable;
+
+namespace Trainer
+{
+    public class GameEvaluationFactory : IEvaluatableFactory
+    {
+        public IEvaluatable Create(INeuralNetwork neuralNetwork)
+        {
+            return new GameEvaluation(new Game(10, 10, 300), neuralNetwork);
+        }
+    }
+}
